c3057382d7f40c6799a3bb49cd4a55cc79d38d41
sh414lim <77113306+sh414lim@users.noreply.github.com>
2021-10-22 15:48:50 +0900

    회원 api

--- nodecat/routes/index.js
+++ nodecat/routes/index.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const axios = require('axios');
+const { response } = require('express');
 
 const router = express.Router();
+
+const URL= 'http://localhost:8002/v1';
+// axios.default.headers.origin='http://localhost:4001';
+
+const request=async(req,api)=>{
+  //요청을 보내는 함수
+  
+}
 
 router.get('/test', async (req, res, next) => { // 토큰 테스트 라우터
   try {
@@ -11,22 +20,28 @@ router.get('/test', async (req, res, next) => { // 토큰 테스트 라우터
       });
       if (tokenResult.data && tokenResult.data.code === 200) { // 토큰 발급 성공
         req.session.jwt = tokenResult.data.token; // 세션에 토큰 저장
+        return await axios.get(`${URL}${api}`,    {
+          headers:{authorization:req.session.jwt},
+        })
       } else { // 토큰 발급 실패
         return res.json(tokenResult.data); // 발급 실패 사유 응답
       }
     }
     // 발급받은 토큰 테스트
-    const result = await axios.get('http://localhost:8002/v1/test', {
-      headers: { authorization : req.session.jwt }, //발급 받은 토큰을 authorization 에 넣어서 테스트를 한다
-    });
-    return res.json(result.data);
+    // const result = await axios.get('http://localhost:8002/v1/test', {
+    //   headers: { authorization : req.session.jwt }, //발급 받은 토큰을 authorization 에 넣어서 테스트를 한다
+    // });
+    // return res.json(result.data);
   } catch (error) {
     console.error(error);
     if (error.response.status === 419) { // 토큰 만료 시
-      return res.json(error.response.data);
+      delete req.session.jwt; // 발급
+      return request (req,api);
     }
-    return next(error);
+    return error.response;
   }
 });
+
+router.get('/my')
 
 module.exports = router;
